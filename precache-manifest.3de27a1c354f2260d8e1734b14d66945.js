abb4b60cffa6954594b1314d987997a9ce9c9813
Eric Hong <erichong@gmail.com>
2020-09-02 21:56:48 -0700

    Updates

---
+++ precache-manifest.3de27a1c354f2260d8e1734b14d66945.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fe0662f4abbae91195623e11e75b76",
+    "url": "/waldo/index.html"
+  },
+  {
+    "revision": "320325bcf9be6eb01c7d",
+    "url": "/waldo/static/css/main.e2606254.chunk.css"
+  },
+  {
+    "revision": "d300b4d757b51a6281e5",
+    "url": "/waldo/static/js/2.33728d12.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/waldo/static/js/2.33728d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "320325bcf9be6eb01c7d",
+    "url": "/waldo/static/js/main.b14e8328.chunk.js"
+  },
+  {
+    "revision": "2749c421917ef3420223",
+    "url": "/waldo/static/js/runtime-main.fbce9503.js"
+  },
+  {
+    "revision": "03a7f67c33c44897db76fa072725df8d",
+    "url": "/waldo/static/media/Alata-Regular.03a7f67c.ttf"
+  },
+  {
+    "revision": "435581927a6ee2efb9fe8c0c6dba7cb2",
+    "url": "/waldo/static/media/odlaw.43558192.png"
+  },
+  {
+    "revision": "5721614a6036fca1c8cfce1a3f1d3f91",
+    "url": "/waldo/static/media/waldo.5721614a.png"
+  },
+  {
+    "revision": "e715f7d4c3947a1547c468dc9fac37b5",
+    "url": "/waldo/static/media/waldo1.e715f7d4.jpg"
+  },
+  {
+    "revision": "973b2d131a8adf119623670746dd7517",
+    "url": "/waldo/static/media/waldo2.973b2d13.jpg"
+  },
+  {
+    "revision": "21c649da5db6ad05a7491e281fa7916f",
+    "url": "/waldo/static/media/waldo3.21c649da.jpg"
+  },
+  {
+    "revision": "e06e29dc531c58f54e2d0438810feed1",
+    "url": "/waldo/static/media/waldo4.e06e29dc.jpg"
+  },
+  {
+    "revision": "4a340c16133025568fe3691731c76e85",
+    "url": "/waldo/static/media/waldo5.4a340c16.jpg"
+  },
+  {
+    "revision": "cbae0d7d7b6c36e0fe492e9e2d6840ce",
+    "url": "/waldo/static/media/waldo6.cbae0d7d.jpg"
+  },
+  {
+    "revision": "23d4855c01fd62ee06dfff509e12667e",
+    "url": "/waldo/static/media/waldo7.23d4855c.jpg"
+  },
+  {
+    "revision": "b66ab7588936cf017eb9643b2f681fbb",
+    "url": "/waldo/static/media/wizard.b66ab758.png"
+  }
+]);
